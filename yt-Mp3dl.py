4bfd46ad4272adaa0941b0981e30874123338b38
Ignorance <81954613+milkyicedtea@users.noreply.github.com>
2023-05-30 22:57:15 +0200

    Fixed! GUI soon? :eyes:
    Will write a README in the following days

--- yt-Mp3dl.py
+++ yt-Mp3dl.py
@@ -1,3 +1,11 @@
+####################
+#                  #
+#     yt-Mp3dl     #
+#                  #
+####################
+
+
+import json
 import os
 from PIL import Image
 from pathlib import Path
@@ -18,29 +26,65 @@ ytdl_format_options = {
 
 ytdl = yt_dlp.YoutubeDL(ytdl_format_options)
 
+class Downloader:
+    def __init__(self):
+        self.directory = Path(str(os.getcwd()))
+        self.downloaded = {
+            "songs": [
 
-def Downloader(url: list):
-    for video in range(len(url)):
-        print(url)
-        print(video)
-        with yt_dlp.YoutubeDL(ytdl_format_options) as ydl:
-            ydl.download(url[video])
-    
-    directory = Path(str(os.getcwd()))
-    print(directory)
-    for filename in directory.iterdir():
-        if filename.suffix == ".webp" or filename.suffix == ".jpg":
-            print(filename.name)
-            image = Image.open(filename)
-            new_dir = Path(str(directory) + "\Covers")
-            if not new_dir.exists():
-                new_dir.mkdir()
-            image.resize([image.height, image.height])
-            image.save(fp = Path(str(new_dir) + "\\" + filename.name[:-len(filename.suffix)] + ".png"), format = "png")
-            filename.unlink()
+            ]
+        }
+
+    def Download(self, url: list[str]):
+        for video in url:
+            print(video)
+            with yt_dlp.YoutubeDL(ytdl_format_options) as ydl:
+                ydl.download(video)
+
+        for filename in self.directory.iterdir():
+            if filename.suffix in [".webp", ".jpg", ".jpeg", ".png"]:
+                # print(filename.name)
+                Cover = Image.open(filename)
+                Cover.resize([Cover.height, Cover.height])
+                cover_dir = Path(str(self.directory) + "\\" + "Covers")
+                if not cover_dir.exists():
+                    cover_dir.mkdir()
+                if Path(str(cover_dir) + "\\" + str(filename.name)).exists():
+                    # filename.rename(str(image_dir) + "\\" + filename.name)
+                    filename.unlink()
+                else:
+                    Cover.save(fp = Path(str(cover_dir) + "\\" + filename.name[:-len(filename.suffix)] + ".png"), format = "png")
+                    filename.unlink()
+
+            elif filename.suffix in [".mp3", ".wav", ".aac", ".ogg"]:
+                # print(filename.name)
+                song_dir = Path(str(self.directory) + "\\" + "Songs")
+                if not song_dir.exists():
+                    song_dir.mkdir()
+                # print(Path(str(song_dir) + str(filename.name)).exists())
+                # print(str(song_dir) + str(filename.name))
+                if Path(str(song_dir) + "\\" + str(filename.name)).exists():
+                    # filename.rename(str(song_dir) + "\\" + filename.name)
+                    filename.unlink()
+                else:
+                    filename.rename(str(song_dir) + "\\" + filename.name)
+                    to_append: dict = {
+                        "name": f"{filename.name[:-len(filename.suffix)]}",
+                        "path": f"{str(song_dir)}\\{str(filename.name)}"
+                    }
+                    self.downloaded["songs"].append(to_append)
+
+    def WriteJson(self):
+        with open("Songs.json", "w") as outfile:
+            json.dump(self.downloaded, outfile, indent = 1)
+            outfile.close()
+        print('All the songs were downloaded and a record can be found in "Songs.Json" ')
+
 
 if __name__ == "__main__":
     txtFile = open('urls.txt', 'r')
     urls = txtFile.readlines()
 
-    Downloader(url = urls)
+    downloader = Downloader()
+    downloader.Download(urls)
+    downloader.WriteJson()
