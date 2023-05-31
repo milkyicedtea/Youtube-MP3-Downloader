8ef92cf8546dcc64cec69203024afbf2e2c78e65
Ignorance <81954613+milkyicedtea@users.noreply.github.com>
2023-05-31 23:03:27 +0200

    So many changes
    Will document tomorrow

--- yt-Mp3dl.py
+++ yt-Mp3dl.py
@@ -5,26 +5,33 @@
 ####################
 
 
-import json
 import os
-from PIL import Image
-from pathlib import Path
 
 import yt_dlp
+
+from pathlib import Path
+
+from PIL import Image
+
+import json
+import yaml
+import itertools
+
 
 ytdl_format_options = {
     'format': 'mp3/bestaudio/best',
     'outtmpl': '/%(title)s.%(ext)s',
     'writethumbnail': 'true',
     'nooverwrites': 'true',
-    'postprocessors': 
-    [{  # Extract audio using ffmpeg
-        'key': 'FFmpegExtractAudio',
-        'preferredcodec': 'mp3',
-    }]
+    'postprocessors':
+        [{  # Extract audio using ffmpeg
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+        }]
 }
 
 ytdl = yt_dlp.YoutubeDL(ytdl_format_options)
+
 
 class Downloader:
     def __init__(self):
@@ -36,16 +43,41 @@ class Downloader:
         }
 
     def Download(self, url: list[str]):
-        for video in url:
-            print(video)
-            with yt_dlp.YoutubeDL(ytdl_format_options) as ydl:
-                ydl.download(video)
+        print(url)
+        print(type(url))
+        with yt_dlp.YoutubeDL(ytdl_format_options) as ydl:
+            if type(url) is not str:
+                for video in url:
+                    print(video)
+                    domains = ["youtube.com/", "music.youtube.com/", "youtu.be/"]
+                    for domain in domains:
+                        if url[0].startswith("https://" + domain):
+                            ydl.download(video)
+                        else:
+                            try:
+                                info = ydl.extract_info(f'ytsearch: {url}', download = False)['entries'][0]
+                                real_url = info['webpage_url']
+                                print(f'real_url {real_url}')
+                                ydl.download(real_url)
+                                break
+                            except:
+                                print('Something went wrong during the download of the file!')
+                                break
+
+
+            else:
+                # real_url = ydl.dl(name = url, info = ydl.extract_info(f'ytsearch:{url}', download = True))
+                info = ydl.extract_info(f'ytsearch: {url}', download = False)['entries'][0]
+                real_url = info['webpage_url']
+                print(f'real_url {real_url}')
+                ydl.download(real_url)
+
 
         for filename in self.directory.iterdir():
             if filename.suffix in [".webp", ".jpg", ".jpeg", ".png"]:
                 # print(filename.name)
                 Cover = Image.open(filename)
-                Cover.resize([Cover.height, Cover.height])
+                Cover = Cover.crop([280, 0, 1000, 720])
                 cover_dir = Path(str(self.directory) + "\\" + "Covers")
                 if not cover_dir.exists():
                     cover_dir.mkdir()
@@ -82,9 +114,53 @@ class Downloader:
 
 
 if __name__ == "__main__":
-    txtFile = open('urls.txt', 'r')
-    urls = txtFile.readlines()
-
     downloader = Downloader()
-    downloader.Download(urls)
-    downloader.WriteJson()
+
+    while True:
+        selection = input("Select how to input you link(s)."
+                          "\nPress 1 to read from file or 2 to input manually. ")
+        match selection:
+            case "1":
+                file_found: bool = False
+                for file in downloader.directory.iterdir():
+                    # print(file)
+                    # print(file.name)
+                    # if file.name[:-len(file.suffix)] in ['urls', 'urls_template']:    # easy access to use templates instead of new files
+                    if file.name[:-len(file.suffix)] in ["urls"]:
+                        file_found = True
+                        match file.suffix:
+                            case ".txt":
+                                urls = open(file, 'r').readlines()
+                                print(f'txt urls {urls}')
+                                downloader.Download(urls)
+
+                            case ".json":
+                                json_data = json.load(open(file))
+                                # print(json_data)
+                                for value in json_data['entries']:
+                                    urls = value['value']
+                                    downloader.Download(urls)
+
+                            case ".yaml":
+                                with open(file, 'r') as yaml_file:
+                                    yaml_data = yaml.full_load(yaml_file)
+                                    # print(yaml_data)
+                                urls = list(itertools.chain(*[data for item, data in yaml_data.items()]))
+                                downloader.Download(urls)
+
+
+                        downloader.WriteJson()
+                if not file_found:
+                    print('You have no file named urls.* in your folder.'
+                          '\nPlease make sure that the file is in the same folder as the program')
+
+                break
+
+            case "2":
+                urls: list = [input("Input the link you want to want to download from. ")]
+                downloader.Download(urls)
+                downloader.WriteJson()
+                break
+
+            case other:
+                print("Invalid input: try re-entering you selection.")
